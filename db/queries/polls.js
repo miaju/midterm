cbd6f252c3e6262f9e3b939946f0d00a896d442e
Mia Tanner <mia@indygecko.com>
2022-11-09 15:02:00 -0800

    make insert functions for choices + polls + votes dbs

--- db/queries/polls.js
+++ db/queries/polls.js
@@ -1,5 +1,9 @@
 const db = require('../connection');
 
+/**
+ * Get polls from the database..
+ * @return {Promise<{}>} A promise to the user.
+ */
 const getPolls = () => {
   return db.query('SELECT * FROM polls;')
     .then(data => {
@@ -7,4 +11,30 @@ const getPolls = () => {
     });
 };
 
-module.exports = { getPolls };
+/**
+ * Add a new poll to the database.
+ * @param {{}} poll
+ * @return {Promise<{}>} A promise to the user.
+ */
+const addPoll = function(poll) {
+  return pool
+    .query(
+      `
+    INSERT INTO polls (creator_email, active, title, description, voter_link, admin_link)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    RETURNING *;
+    `,
+      [poll.creator_email, poll.active, poll.title, poll.description, poll.voter_link, poll.admin_link]
+    )
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+module.exports = {
+  getPolls,
+  addPoll,
+};
