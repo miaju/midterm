d365477d94be003040a6901ec25eb40b07c1bc65
Mia Tanner <mia@indygecko.com>
2022-11-16 20:50:33 -0800

    remove unused code

--- routes/vote.js
+++ routes/vote.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router  = express.Router();
 
 const pollsQueries = require('../db/queries/polls');
-const choiceQueries = require('../db/queries/choices');
 
 router.get('/:id', (req, res) => {
   pollsQueries.getPollByLink(req.params.id)
