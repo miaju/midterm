2985aa66d9b49ee7f3537e457d63ee0e53ed6fe4
Mia Tanner <mia@indygecko.com>
2022-11-16 20:51:31 -0800

    refactor + clean up the admin/:id route

--- routes/admin.js
+++ routes/admin.js
@@ -1,41 +1,36 @@
 const express = require('express');
 const router  = express.Router();
 const pollsQueries = require('../db/queries/polls');
+const choiceQueries = require('../db/queries/choices');
 
-router.get('/:id',(req,res)=>{
-  const link = req.params.id;
-  console.log(link);
-  pollsQueries.getPollByLink(link)
-    .then((result)=>{
-      if (result) {
-        if (link == result.voter_link) {
-          res.redirect(`/vote/:${link}`);
-        } else if (link != result.admin_link) {
-          const msg = "The link provide is not valid!";
-          res.render("/msg",{msg});
-        } else {
-          const root = "localhost:8080/";
-          const pollid = result.id;
-          const title = result.
-            choiceQueries.getChoicesandscore(pollid)
-            .then((result)=>{
-              /*
-          result output like below:
-          [
-              { choice_id: 4, value: 'sushi', score: '16' },
-              { choice_id: 5, value: 'ramen', score: '13' },
-              { choice_id: 6, value: 'sashimi', score: '11' }
-           ]
-          */
-              const templateVars = {
-                admin_link:root.concat('admin/:',poll.admin_link),
-                voter_link:root.concat('voter/:',poll.voter_link),
-                choicesandscore:result
-              };
-              res.render(`/:${link}`,templateVars);
-            });
-        }
-      }
+
+router.get('/:id', (req, res) => {
+  pollsQueries.getPollByLink(req.params.id)
+    .then((poll) => {
+      const root = "localhost:8080/";
+      const pollId = poll.id;
+      choiceQueries.getChoicesandscore(pollId)
+        .then((choices) => {
+          console.log(`choices: ${choices}`);
+          const templateVars = {
+            admin_link: root.concat('admin/:', poll.admin_link),
+            voter_link: root.concat('voter/:', poll.voter_link),
+            poll, //have all elements of polls in the db
+            choices // have choice_id, value, and score
+          };
+          console.log(templateVars);
+          res.render('admin', templateVars);
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
     });
 });
 
