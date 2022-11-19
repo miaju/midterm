5ee5cdce6faef14b75fdd1699f188f091a14c448
Efe Erkmen <efeerkmen@gmail.com>
2022-11-19 15:18:14 +0000

    added copy to clipboard button function to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -17,3 +17,37 @@ $(() => {
     });
   });
 });
+function adminCopy() {
+  // Get the text field
+  var copyText = document.getElementById("adminlink");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
+
+function voterCopy() {
+  // Get the text field
+  var copyText = document.getElementById("voterlink");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
+
+module.exports = {
+  adminCopy,
+  voterCopy,
+};
